0b50416f3f06f34fd75c2b884ab0eaea19515ff5
David LeBlanc <65300409+davidal00@users.noreply.github.com>
2020-05-22 14:06:34 -0400

    assignment complete

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 // TODO: add code here
+window.addEventListener("load", function () {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function (response) {
+        response.json().then(function (json) {
+            const div = document.getElementById("container");
+            document.addEventListener("click", function () {
+
+                for (let i = 0; i < json.length; i++) {
+
+                    div.innerHTML = div.innerHTML + `
+                        <div class="astronaut">
+                            <div class="bio">
+                                <h3>${json[i].firstName} ${json[i].lastName}</h3>
+                                <ul>
+                                    <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                    <li>Active: ${json[i].active}</li>
+                                    <li>Skills: ${json[i].skills}</li>
+                                </ul>
+                            </div>
+                            <img src=${json[i].picture} class="avatar" />
+                        </div>
+                    `;
+                };
+            });
+        });
+    });
+});
